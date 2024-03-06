67b8d0fd8314f661ab7dc38cb290659817325b75
mr.mk1950 <153270610+MANISHKAVAIYA@users.noreply.github.com>
2024-03-06 22:17:32 +0530

    java

---
+++ swing_pro/src/demo1_swing.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.lang.*;
+import java.io.*;
+import javax.swing.*;
+
+public class demo1_swing {
+
+    public static void main(String[] args) {
+
+    }
+}
