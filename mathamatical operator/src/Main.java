d20811a390c58279b2043814a18ee8d50bf005b9
mr.mk1950 <153270610+MANISHKAVAIYA@users.noreply.github.com>
2024-03-07 12:20:49 +0530

    java

---
+++ mathamatical operator/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int a,b;
+        double c,d;
+        //int value
+        a=10/2;
+        b=10%2;
+        //double value
+        c=10/2.0;
+        d=10%2.0;
+        System.out.println("Answer of 10/2 : " + a + " Answer of 10/2.0 : " +c);
+        System.out.println("Answer of 10%2 : " + b + " Answer of 10%2.0 : " +d);
+
+        // find the factor of value
+
+    }
+}
