4c7adcf2434ab05e6eaab0cb1c809f1711ddd521
Manish <153270610+MANISHKAVAIYA@users.noreply.github.com>
2024-03-16 02:53:51 +0530

    Update main.java

--- practice code part 1/main.java
+++ practice code part 1/main.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package unit_1;
 
-/**
- *
- * @author ADMIN
- */
 class main {
-    
+    static void main(String[] args){
+        System.out.print("Demo file 1");
+    }
 }
