2715dd011140e6ae72fdf3e9ddf3b2a7ff164942
mr.mk1950 <153270610+MANISHKAVAIYA@users.noreply.github.com>
2024-03-16 02:51:40 +0530

    java

---
+++ practice code part 1/hello_world.java
@@ -0,0 +1,9 @@
+package unit_1;
+
+public class hello_world {
+
+    public static void main(String args[]) {
+        System.out.println("Hello Java");
+        System.out.println("Welcome to Coding.");
+    }
+}
---
+++ practice code part 1/java_variables.java
@@ -0,0 +1,16 @@
+package unit_1;
+
+public class java_variables {
+
+    public static void main(String args[]) {
+        int a = 23;
+        String b = "Hello Java";
+        float c = 22.9f;
+
+//      print the variable
+        System.out.println("print a : " + a);
+        System.out.println("print b : " + b);
+        System.out.println("print c : " + c);
+        
+    }
+}
